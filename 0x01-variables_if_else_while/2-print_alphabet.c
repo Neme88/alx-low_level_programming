e65ac4fcfcb93bc85087d0e5b57328a4e669e1eb
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2022-12-10 00:45:21 +0200

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - print alphbet A-Z in lowercase.
+*
+* Return:0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	return (0);
+}
