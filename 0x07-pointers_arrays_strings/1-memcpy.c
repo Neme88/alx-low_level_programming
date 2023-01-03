5ef91a944fce4ec1fcd7c3cb322c47df734a5266
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-01-03 20:13:17 +0200

    1-memcpy.c project

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,20 @@
 #include "main.h"
-
 /**
-* main - copy memory area.
-* @dest: copy
-* @src: original address
-* @n: limit
-* Return: pointer
-*/
+ * _memcpy - fills memory with another buffer.
+ * @dest: source string
+ * @src: string for filling
+ * @n: lenght of buffer
+ * Return: new string dest.
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i = 0;
 
-	char *b = src;
-	char *a = dest;
-
-	while (n != 0)
+	while (i < n)
 	{
-		*a++ = *b++;
-		n--;
+		*(dest + i) = *(src + i);
+		i++;
 	}
-
 	return (dest);
-
 }
 
