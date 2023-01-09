31c27565aff5cbd5cf836a6a0fc73d35e9c59ffe
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-01-09 18:42:28 +0200

    4-add.c project

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,31 +2,29 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/**
- * main - Entry point function
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
- */
-int main(int argc, char **argv)
-{
-	int i, n, ex;
 
-	ex = 0, n = 0;
-	if (argc > 1)
+/**
+ * _isnumber - checks if string is a number
+ * @s: string
+ *
+ * Return: On success 1.
+ * If not a number, 0 is returned.
+ */
+int _isnumber(char *s)
+{
+	int i, check, d;
+
+	i = 0, d = 0, check = 1;
+	if (*s == '-')
+		i++;
+	for (; *(s + i) != 0; i++)
 	{
-		for (i = 1; i < argc; i++)
+		d = isdigit(*(s + i));
+		if (d == 0)
 		{
-			if (_isnumber(argv[i]))
-				n += atoi(argv[i]);
-			else
-				ex = 1;
+			check = 0;
+			break;
 		}
 	}
-	if (ex == 0)
-		printf("%i\n", n);
-	else
-		printf("%s\n", "Error");
-	return (ex);
+	return (check);
 }
