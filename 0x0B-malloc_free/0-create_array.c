8bd5ea1acd4c107375fb85ee0a4a0d3d4c1ef7d3
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-01-10 16:56:01 +0200

    0-creeate_array.c project

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,14 +15,19 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
+	char *s;
 
 	if (size == 0)
-		return (NULL);
+		return (0);
 
-	char *s = (char *)malloc(size * sizeof(char));
+	s = malloc(sizeof(char) * size);
 
+	if (s == 0)
+		return (0);
 	for (i = 0; i < size; i++)
-		s[i] = c;
+		*(s + i) = c;
+
+	*(s + i) = '\0';
 
 	return (s);
 }
