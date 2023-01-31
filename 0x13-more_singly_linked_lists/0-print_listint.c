3335b32138e660530d3518bddc908a3dae2c7c78
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2023-01-31 17:55:10 +0200

    solved 1-listint_len.c project

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,23 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
- * Return: number of nodes
+ * Return: The number of nodes in a list.
  */
-size_t listint_len(const listint_t *h)
+
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
 	while (h)
 	{
-		num++;
+		nodes++;
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (num);
+	return (nodes);
 }
